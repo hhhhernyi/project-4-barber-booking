316c0f08d77283b9f4263b38817c10f1bfe7a521
Hern Yi Lee <hyileenet@gmail.com>
2025-03-17 09:54:27 +0800

    fix errors 2

--- project4-frontend/src/main.js
+++ project4-frontend/src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import router from '../src/router/index'
 import toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css';
-import Carousel3d from 'vue-carousel-3d'
 
 
 const app = createApp(App)
