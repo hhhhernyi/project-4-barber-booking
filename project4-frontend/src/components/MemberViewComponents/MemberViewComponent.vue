82e22bba025d3cf5ccadfde367589a34e3d5f99f
Hern Yi Lee <hyileenet@gmail.com>
2025-03-18 10:03:55 +0800

    member page

--- project4-frontend/src/components/MemberViewComponents/MemberViewComponent.vue
+++ project4-frontend/src/components/MemberViewComponents/MemberViewComponent.vue
@@ -5,6 +5,8 @@ const userId = decoded.payload._id
 console.log(decoded.payload.admin)
 import { onMounted, reactive, ref } from 'vue';
 import * as userService from '../../services/user'
+import dayjs from "dayjs";
+dayjs().format()
 // IMPORTS
 // CONSTANTS
 const state = reactive({
@@ -29,11 +31,14 @@ onMounted(async ()=>{
 
 <template>
     <div v-if="token" class="w-full h-[800px] flex flex-col items-center justify-center">
-        <h1>Member Info</h1>
-        <p>Email: {{ state.userDetail.email }}</p>
-        <p>Contact: {{ state.userDetail.mobileNumber }}</p>
-        <p>Points: {{ state.userDetail.points }}</p>
-       <p>Member Since: {{ state.userDetail.createdAt }}</p>
+        <div class="rounded-2xl bg-off-white w-[80%] max-w-[400px] flex flex-col justify-center items-center p-3 shadow-2xl">
+            <h1 class="text-2xl font-special">Member Info</h1>
+            <h2>Email: </h2><p class="border-[2px] px-2 rounded-lg border-gray-400">{{ state.userDetail.email }}</p>
+            <h2>Contact: </h2><p class="border-[2px] px-2 rounded-lg border-gray-400">{{ state.userDetail.mobileNumber }}</p>
+            <h2>Points: </h2><p class="border-[2px] px-2 rounded-lg border-gray-400">{{ state.userDetail.points }}</p>
+            <h2>Member Since: </h2><p class="border-[2px] px-2 rounded-lg border-gray-400">{{  dayjs(state.userDetail.createdAt).toString().slice(0,17) }}</p>
+        </div>
+        
         
     </div>
 
